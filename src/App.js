c112d523dadab1199590534b3bf2a7d2a0411b32
dmkhomen <dmkhomen@gmail.com>
2022-11-12 20:11:31 +0200

    footer, navbar, routes structure + dark mode

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import { Routes } from "./components/Routes";
 
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
-
   return (
     <div className={darkTheme ? "dark" : ""}>
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
-        <Navbar />
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
         <Routes />
         <Footer />
       </div>
