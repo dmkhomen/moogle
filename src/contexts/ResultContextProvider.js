6e06aa0d6ea18767d9c69bd0d7ee23371a5185b9
dmkhomen <dmkhomen@gmail.com>
2022-11-12 23:12:54 +0200

    routes

---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,6 @@
+import React, { createContext, useContext, useState } from "react";
+
+const ResultContext = createContext();
+const baseUrl = "https://g-search.p.rapidapi.com/search";
+
+export const ResultContextProvider = ({ children }) => {};
