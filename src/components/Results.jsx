28cbfd86ca02a81d778fc2861a9cefcbf25b97b1
dmkhomen <dmkhomen@gmail.com>
2022-11-13 17:05:08 +0200

    news, images, videos with fake data

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -21,10 +21,6 @@ export const Results = () => {
   
   switch (location.pathname) {
     case "/search":
-
-
-  console.log (results.data.organic_results[0].title)
-
       return (
         <div className="flex flex-wrap justify-between sm:px-56">
 
@@ -43,7 +39,22 @@ export const Results = () => {
         </div>
       );
     case "/news":
-      return 'NEWS';
+      return (
+        <div className="flex flex-wrap justify-between sm:px-56 items-center">
+
+          {results?.data?.news?.map(({url, title}, index) => (
+            <div key={index} className="md:w-1/2 w-full mb-6">
+              <a href={url} target="_blank" rel="noreferrer" className="hover:underline">
+                <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
+                  {title}
+                </p>
+                <div className="flex gap-4"></div>
+              </a> 
+            </div>
+          ))}
+
+        </div>
+      );
     case "/images":
       return (
         <div className="flex flex-wrap justify-center items-center">
@@ -58,7 +69,15 @@ export const Results = () => {
         </div>
       );
     case "/videos":
-      return 'VIDEOS';
+      return (
+        <div className="flex flex-wrap">
+          {results?.data?.video_results?.map((video, index) => (
+            <div key={index} className="p-2">
+              <ReactPlayer url={video.additional_links[0].href} controls width="355px" height="200px" />
+            </div>
+          ))}
+        </div>
+      );
   
     default:
       return "ERROR";
